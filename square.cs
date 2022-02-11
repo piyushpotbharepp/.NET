3a83dbd3e468264c4cd689afb741ccb14ea2baff
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-11 13:48:16 +0530

    WAP To Print Square Of The Number

---
+++ square.cs
@@ -0,0 +1,23 @@
+﻿// Write a program to print the square of the number.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5, sq;
+            sq = num * num;
+            Console.WriteLine("Square : " + sq);
+            Console.ReadLine(); 
+
+
+        }
+    }
+}
