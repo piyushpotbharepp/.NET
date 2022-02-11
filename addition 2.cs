d411b9ff44bfeb9746d3127b77b96bdbad1cc5b5
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-11 14:04:33 +0530

    WAP To Print The Addition Of Two Numbers

---
+++ addition 2.cs
@@ -0,0 +1,23 @@
+﻿// Write a program to print the addition of two numbers.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 13,num2 = 11,addition;
+            addition = num1 + num2; 
+            Console.WriteLine("Addition : " + addition);
+            Console.ReadLine(); 
+
+
+        }
+    }
+}
