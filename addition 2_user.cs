832fffca44d0450aa230579549055d9350ca0547
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-11 13:59:22 +0530

    WAP To Accept 2 Nos. From User & Print The Addition

---
+++ addition 2_user.cs
@@ -0,0 +1,28 @@
+﻿// Write a program to accept 2 numbers from user
+// & print the addition of the number.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1,num2,addition;
+            Console.WriteLine("Enter the 1st Number :");
+            num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the 2nd Number :");
+            num2=Convert.ToInt32(Console.ReadLine());   
+            addition = num1 + num2; 
+            Console.WriteLine("Addition : " + addition);
+            Console.ReadLine(); 
+
+
+        }
+    }
+}
