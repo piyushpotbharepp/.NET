628b99aeceb8cf60a8b978ccec778aabd80bc5b3
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-11 13:43:24 +0530

    WAP To Program To Accept No. From User & Print Square

---
+++ square_user.cs
@@ -0,0 +1,25 @@
+﻿// Write a program to accept number from the user and print the square.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, sq;
+            Console.WriteLine("Enter Number :");
+            num = Convert.ToInt32(Console.ReadLine());
+            sq = num * num;
+            Console.WriteLine("Square : " + sq);
+            Console.ReadLine(); 
+
+
+        }
+    }
+}
