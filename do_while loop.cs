cb0b856fec548cefe07fd6c7813c82a139f0353a
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-14 11:36:15 +0530

    Understanding Do While Loop

---
+++ do_while loop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Feb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            i = 1;
+            do
+            {
+                Console.WriteLine("Value Of i = " + i);
+                i++;
+            } while (i <= 25)
+
+            Console.ReadLine();
+        }
+    }
+}
