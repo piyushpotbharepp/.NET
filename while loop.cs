dbe3e4a5aee170eb911e58968999f86a0502573c
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-14 11:34:54 +0530

    Understanding While Loop

---
+++ while loop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Feb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            i = 1;
+            while(i<=25)
+            {
+                Console.WriteLine("Value Of i = " + i);
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
