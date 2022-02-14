16ad48de00bbe06d5078f13147b434db138aef22
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-14 11:32:01 +0530

    Understanding For Loop

---
+++ for loop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Feb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for(i=1;i<=25;i++)
+            {
+                Console.WriteLine("Value Of i = " + i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
