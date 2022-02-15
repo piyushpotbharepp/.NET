6cbe7a0aae813c72b0cee6bd67a39cbf789a5192
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-15 13:35:33 +0530

    Program To Find Area Of Trapezoid

---
+++ Area of trapezoid.cs
@@ -0,0 +1,30 @@
+﻿// Write a program to print the area of the trapezoid
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int b1 = 4, b2 = 3, h = 3, area;
+
+            Console.WriteLine("================================================================");
+            Console.WriteLine("AREA OF TRAPEZOID");
+            Console.WriteLine("================================================================");
+
+            area = ((b1 + b2)/2) * h;
+
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("Area of the Trapezoid : " + area);
+            Console.WriteLine("----------------------------------------------------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
