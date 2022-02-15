974564959ee6080660e3a8e48131165704d5d61f
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-15 13:07:10 +0530

    Program To Find Area Of Rectangle From User Input

---
+++ Area of rect from user input.cs
@@ -0,0 +1,36 @@
+﻿// Write a program to accept length and breadth of a rectangle
+// and print the area of the rectangle
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int l, b, area;
+
+            Console.WriteLine("================================================================");
+            Console.WriteLine("AREA OF RECTANGLE");
+            Console.WriteLine("================================================================");
+
+            Console.WriteLine("Enter the length of the rectangle : ");
+            l=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the breadth of the rectangle : ");
+            b=Convert.ToInt32(Console.ReadLine());
+           
+            area = l*b;
+
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("Area of the rectangle : " + area);
+            Console.WriteLine("----------------------------------------------------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
