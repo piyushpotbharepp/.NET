b64acb30648efadd4982e3b264cdac35ca722538
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-15 12:56:37 +0530

    Program To Find Addition Of 3 Nos

---
+++ Add 3 nos.cs
@@ -0,0 +1,23 @@
+﻿// Write a program to find the addition of three numbers.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num1 = 5, num2 = 6, num3 = 8, addition;
+           
+            addition = num1 + num2 + num3;
+
+            Console.WriteLine("Addition Of Numbers : " + addition);
+            Console.ReadLine();
+        }
+    }
+}
