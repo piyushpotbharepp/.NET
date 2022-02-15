b670964b5fae0e02b01ff924828618b5423d5e03
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-15 13:50:42 +0530

    Program To Find Area Of Circle From User Input

---
+++ Area of circle from user input.cs
@@ -0,0 +1,35 @@
+﻿// Write a program to accept radius of circle
+// and print the area of the circle
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int r; 
+            double area;
+
+            Console.WriteLine("================================================================");
+            Console.WriteLine("AREA OF CIRCLE");
+            Console.WriteLine("================================================================");
+
+            Console.WriteLine("Enter the Radius of the circle : ");
+            r = Convert.ToInt32(Console.ReadLine());   
+
+            area = 3.14 * r * r;
+
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("Area of the Circle : " + area);
+            Console.WriteLine("----------------------------------------------------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
