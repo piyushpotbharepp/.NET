d18cc1ee6a951500c97951e0d9a24efc828bd7bb
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-15 13:31:20 +0530

    Program To Find Area Of Triangle From User Input

---
+++ Area of triangle from user input.cs
@@ -0,0 +1,36 @@
+﻿// Write a program to accept base and height of triangle
+// and print the area of the triangle
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int b, h, area;
+
+            Console.WriteLine("================================================================");
+            Console.WriteLine("AREA OF TRIANGLE");
+            Console.WriteLine("================================================================");
+
+            Console.WriteLine("Enter the Base of the triangle : ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Height of the triangle : ");
+            h = Convert.ToInt32(Console.ReadLine());    
+
+            area = (b*h)/2;
+
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("Area of the Triangle : " + area);
+            Console.WriteLine("----------------------------------------------------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
