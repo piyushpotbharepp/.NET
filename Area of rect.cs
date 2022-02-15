ef1d6e22c737af5b5610db51b088f6aa71807ff0
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-15 13:09:28 +0530

    Program To Find Area Of Rectangle

---
+++ Area of rect.cs
@@ -0,0 +1,30 @@
+﻿// Write a program to print the area of the rectangle
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int l = 12, b = 8, area;
+
+            Console.WriteLine("================================================================");
+            Console.WriteLine("AREA OF RECTANGLE");
+            Console.WriteLine("================================================================");
+
+            area = l*b;
+
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("Area of the rectangle : " + area);
+            Console.WriteLine("----------------------------------------------------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
