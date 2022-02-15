e3dea3ec9c6a81afc16192ad1aa971986051a477
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-15 15:02:16 +0530

    Program To Find Volume Of Prisms From User Input

---
+++ Volume of prisms from user input.cs
@@ -0,0 +1,36 @@
+﻿// Write a program to accept base and height of prisms
+// and print the volume of the prisms
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double b, h, vol;
+
+            Console.WriteLine("================================================================");
+            Console.WriteLine("VOLUME OF PRISMS");
+            Console.WriteLine("================================================================");
+
+            Console.WriteLine("Enter the Base of the prisms : ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Height of the prisms : ");
+            h = Convert.ToInt32(Console.ReadLine());
+
+            vol = b * h;
+
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("Volume of the Prisms : " + vol);
+            Console.WriteLine("----------------------------------------------------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
