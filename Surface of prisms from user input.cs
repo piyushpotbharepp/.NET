2abe844285b06ffb361eae7be2735eb758bfce17
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-15 14:19:34 +0530

    Program To Find Surface Of Prisms From User Input

---
+++ Surface of prisms from user input.cs
@@ -0,0 +1,38 @@
+﻿// Write a program to accept base, perimeter and height of prisms
+// and print the surface of the prisms
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double b, p, h, area;
+
+            Console.WriteLine("================================================================");
+            Console.WriteLine("SURFACE OF PRISMS");
+            Console.WriteLine("================================================================");
+
+            Console.WriteLine("Enter the Base of the prisms : ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Perimeter of the prisms : ");
+            p = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Height of the prisms : ");
+            h = Convert.ToInt32(Console.ReadLine());
+
+            area = (2 * b) + (p * h);
+
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("Surface of the Prisms : " + area);
+            Console.WriteLine("----------------------------------------------------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
