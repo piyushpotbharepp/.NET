31864fb4aff83324934ee978e7e109358498caa5
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-15 14:27:46 +0530

    Program To Find Surface Of Cylinder From User Input

---
+++ Surface of cylinder from user input.cs
@@ -0,0 +1,36 @@
+﻿// Write a program to accept radius and height of cylinder
+// and print the surface of the cylinder
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double r, h, area;
+
+            Console.WriteLine("================================================================");
+            Console.WriteLine("SURFACE OF CYLINDER");
+            Console.WriteLine("================================================================");
+
+            Console.WriteLine("Enter the Radius of the cylinder : ");
+            r = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Height of the cylinder : ");
+            h = Convert.ToInt32(Console.ReadLine());
+
+            area = (2 * 3.14 * r * h) + (2 * 3.14 * r * r);
+
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("Surface of the Cylinder : " + area);
+            Console.WriteLine("----------------------------------------------------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
