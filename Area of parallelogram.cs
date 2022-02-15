4aaae7844698dc77e528aa3c2496578e566d27a8
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-15 13:21:46 +0530

    Program To Find Area Of Parallelogram

---
+++ Area of parallelogram.cs
@@ -0,0 +1,30 @@
+﻿// Write a program to print the area of the parallelogram
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int b = 4, h = 3, area;
+
+            Console.WriteLine("================================================================");
+            Console.WriteLine("AREA OF PARALLELOGRAM");
+            Console.WriteLine("================================================================");
+
+            area = b * h;
+
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("Area of the Parallelogram : " + area);
+            Console.WriteLine("----------------------------------------------------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
