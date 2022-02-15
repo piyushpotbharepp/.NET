75d4cde6d2231ce8522eeb03c595abdc139423b1
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-15 15:17:24 +0530

    Program To Find Perimeter Of Rectangle

---
+++ Perimeter of rectangle.cs
@@ -0,0 +1,30 @@
+﻿// Write a program to print the perimeter of the rectangle
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double l = 12, b = 8, peri;
+
+            Console.WriteLine("================================================================");
+            Console.WriteLine("PERIMETER OF RECTANGLE");
+            Console.WriteLine("================================================================");
+
+            peri = 2*l * 2*b;
+
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("Perimeter of the rectangle : " + peri);
+            Console.WriteLine("----------------------------------------------------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
