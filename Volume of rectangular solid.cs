075316b491ff8d62b0edec1d7a4d636b98e32c6f
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-15 15:05:26 +0530

    Program To Find Volume Of Rectangular Solid

---
+++ Volume of rectangular solid.cs
@@ -0,0 +1,30 @@
+﻿// Write a program to print the volume of the rectangular solid
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double l = 20, w = 10, h = 25, vol;
+
+            Console.WriteLine("================================================================");
+            Console.WriteLine("VOLUME OF RECTANGULAR SOLID");
+            Console.WriteLine("================================================================");
+
+            vol = l * w * h;
+
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("Volume of the Rectangular Solid : " + vol);
+            Console.WriteLine("----------------------------------------------------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
