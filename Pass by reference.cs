bb6474e937d1f2ad9739f10480f45f00db95502e
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-16 15:42:14 +0530

    Understanding Pass By Reference

---
+++ Pass by reference.cs
@@ -0,0 +1,30 @@
+﻿// Understanding Pass By Reference
+// >>  Changes made in the method affect the caller  
+// >>  Assign parameter value before calling the method
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+   class Program
+   {
+            static void AddOne(ref int x)
+            {
+                x++;
+                Console.WriteLine("Value inside AddOne Method is : " + x);//Output will be 6
+            }
+            static void Main()
+            {
+                int p = 5;
+                AddOne(ref p);
+                Console.WriteLine("Value after calling the method in Main is : " + p);//Output will be 6
+                Console.ReadLine();
+            }
+
+   }
+
+}
