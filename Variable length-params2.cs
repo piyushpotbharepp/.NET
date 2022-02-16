a971506d8e63e5273dd7d9b5563f242583093c00
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-16 20:56:25 +0530

    Understanding Using Variable Length Parameter Lists

---
+++ Variable length-params2.cs
@@ -0,0 +1,32 @@
+﻿// Understanding Using Variable Length Parameter List 
+// >>  Use the params keyword
+// >>  Declare as an array at the end of the parameter  list
+// >>  Always pass by value
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static long AddList(params long[] v)
+        {
+            long total, i;
+            for(i= 0, total=0; i < v.Length; i++)
+            {
+                total += v[i];
+            }
+            return total;
+        }
+        static void Main()
+        {
+            long x = AddList(45, 73, 56, 63, 43, 67, 93);
+            Console.WriteLine("Value Of Variable X : " + x);
+            Console.ReadLine();
+        }
+    }
+}
