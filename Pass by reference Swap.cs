2267ab0035fa358b2af7e6f445b64759cc1b9868
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-16 16:26:44 +0530

    Understanding Pass By Reference - Swap Numbers

---
+++ Pass by reference Swap.cs
@@ -0,0 +1,34 @@
+﻿// Understanding Pass By Reference - Swap
+// >>  Changes made in the method affect the caller  
+// >>  Assign parameter value before calling the method
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void SwapByRef(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+        static void Main()
+        {
+            int i = 2, j = 3;
+            System.Console.WriteLine("i = {0}  j = {1}", i, j);
+
+            SwapByRef(ref i, ref j);
+
+            System.Console.WriteLine("i = {0}  j = {1}", i, j);
+
+            System.Console.ReadKey();
+        }
+    }
+
+}
