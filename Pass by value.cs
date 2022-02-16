89a3d083668be436497f4a3a0efe8743a0d26cb4
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-16 14:52:43 +0530

    Understanding Pass By Value

---
+++ Pass by value.cs
@@ -0,0 +1,29 @@
+﻿// Understanding Pass By Value 
+// >>  Variable can be changed inside the method 
+// >>  Has no effect on value outside the method
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void AddOne(int x)
+        {
+            x++;
+            Console.WriteLine("Value inside AddOne Method is : " + x);//Output will be 6
+        }
+        static void main()
+        {
+            int p = 5;
+            AddOne(p);
+            Console.WriteLine(p);//Output will be 5
+        }
+
+    }
+
+}
