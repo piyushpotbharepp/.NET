29baf6170fb9d043b4e2cf1b2dce225d801e4146
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-16 20:08:30 +0530

    Understanding Out Parameter

---
+++ Out Parameter1.cs
@@ -0,0 +1,31 @@
+﻿// Understanding Out Parameter 
+// >>  Values are placed out but not in  
+// >>  Like ref, but values are not passed into the method
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void AddOne(out int x)
+        {
+            x = 10;
+            x++;
+            Console.WriteLine("Rate Of Intrest : " + x);
+            Console.ReadLine();
+        }
+        static void Main()
+        {
+            int roi;
+            AddOne(out roi);
+            Console.WriteLine("Rate Of Intrest : " + roi);
+            Console.ReadLine();
+
+        }
+    }
+}
