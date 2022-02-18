a3347d653b600d1f01e9ad2d41fd4315053ca28f
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-18 19:30:02 +0530

    Understanding Array Rank Property For One Dimensional Array

---
+++ Two Dimensional Array Rank.cs
@@ -0,0 +1,26 @@
+﻿/* Understanding Rank Property[ Two Dimensional ] 
+--------------------------------------------------------------
+>>  The rank is the numbers of dimensions of an array 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[,] arr2 = new int[4, 8];
+
+            Console.WriteLine("Dimesion Of Array : " + arr2.Rank);
+
+            Console.ReadLine();
+        }
+
+    }
+}
