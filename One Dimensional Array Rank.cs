904b8db1ffcc9c2ce8df38d557112a1631b7dff2
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-18 19:12:46 +0530

    Understanding Array Rank Property For One Dimensional Array

---
+++ One Dimensional Array Rank.cs
@@ -0,0 +1,28 @@
+﻿/* Understanding Rank Property[ One Dimensional ] 
+--------------------------------------------------------------
+>>  The rank is the numbers of dimensions of an array 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] weekDays;
+
+            weekDays = new string[] { "Monday", "Tuesday", "Wednessday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            Console.WriteLine("Dimesion Of weekDays Array : " + weekDays.Rank);
+
+            Console.ReadLine();
+        }
+
+    }
+}
