edef51af47399751010aa843bfc274d703d506d7
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-18 19:47:00 +0530

    Understanding Array Rank Property For Jagged Array

---
+++ Jagged Array1.cs
@@ -0,0 +1,25 @@
+﻿/* Understanding Rank Property[ Jagged Array ] 
+--------------------------------------------------------------
+>>  The rank is the numbers of dimensions of an array 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[][] array = new int[2][];
+            Console.WriteLine("Dimesion Of Jdarr : " + array.Rank);
+
+            Console.ReadLine();
+        }
+
+    }
+}
