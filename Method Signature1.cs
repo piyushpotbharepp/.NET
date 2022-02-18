80bc906a8bcfd5a3695eaa10767507abb192a918
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-18 15:51:25 +0530

    Understanding Method Signatures

---
+++ Method Signature1.cs
@@ -0,0 +1,41 @@
+﻿/* Understanding Method Signatures  
+--------------------------------------------------------------
+>>  Method must be unique within the class 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static int Add(int a)
+        {
+            return a;
+        }
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static long Add(int a, long b)
+        {
+            return a + b;
+        }
+       // Same methods with same parameters can not be called. 
+       // It will show an error.
+       // You have to change the prameter to run the code.
+        static void Main()
+        {
+            Console.WriteLine("Addition of 1 Number : " + Add(1));
+            Console.WriteLine("Addition of 2 Numbers : " + Add(22, 32));
+            Console.WriteLine("Addiyion of 3 Numbers : " + Add(55, 23L));
+            
+            Console.ReadLine();
+        }
+    }
+
+}
