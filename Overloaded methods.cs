558cbb09aa480fc5196d755b74a33459d425decd
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-18 14:21:58 +0530

    Understanding The Overloaded Methods

---
+++ Overloaded methods.cs
@@ -0,0 +1,36 @@
+﻿/* Understanding the Overloaded Methods 
+--------------------------------------------------------------
+>>  Method overloading is the common way of implementing polymorphism
+    It is the ability to redefine a function in more than one form.
+>>  A user can implement function overloading by defining two or 
+    more functions in a class sharing the same name. 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static int Add(int a, int b, int c)
+        {
+            return (a + b) + c;
+        }
+        static void Main()
+        {
+            Console.WriteLine("Addition of 2 Numbers : " + Add(22, 32));
+            Console.WriteLine("Addiyion of 3 Numbers : " + Add(55, 23, 32));
+
+            Console.ReadLine();
+        }
+    }
+
+}
