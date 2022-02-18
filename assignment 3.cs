ed2e9d3a806655d519951a9f5b3f1e945ea0f6a0
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-18 10:49:28 +0530

    Create A Class Circle & Methods Circlearea And CircleCircumference

---
+++ assignment 3.cs
@@ -0,0 +1,52 @@
+﻿//2- create a class cirlce having fields radius and
+//method calculatearea and calculatecirc
+
+using System;
+
+namespace circle
+{
+    class circle
+    {
+        float radius;
+        float area;
+        float circ;
+
+
+        public void areaa(float radius)
+        {
+            area = 3.14f * radius * radius;
+        }
+        public void dispareaa()
+        {
+            Console.WriteLine("Area Of Circle =" + area);
+        }
+        public void circc(float radius)
+        {
+            circ = 2 * 3.14f * radius;
+        }
+        public void dispcircc()
+        {
+            Console.WriteLine("Circumference Of Circle =" + circ);
+        }
+    }
+
+    class @circlee
+    {
+        static void Main()
+        {
+            circle cir = new circle();
+            Console.WriteLine("Enter Radius");
+            float radius = Convert.ToSingle(Console.ReadLine());
+            cir.areaa( radius);
+            cir.dispareaa();
+            cir.circc( radius);
+            cir.dispcircc();
+
+            Console.ReadLine();
+
+
+        }
+    }
+
+
+}
