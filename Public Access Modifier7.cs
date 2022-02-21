0e49e53e2c4d40fc2299c84fb8b34f09a718a98b
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-02-21 15:06:43 +0530

    Understanding Access Modifier - Public

---
+++ Public Access Modifier7.cs
@@ -0,0 +1,45 @@
+﻿/*
+================================================
+              Access Modifiers 
+================================================
+           Public Access Modifier
+------------------------------------------------
+ >> Public is not restricted.
+ >> It specifies that access is not restricted.
+ >> Objects that implement public access modifiers are accessible from 
+    everywhere in our project, therefore there is no accessibility restrictions.
+.................................................
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Details
+    {
+        public string Name = "Understanding Access Modifier - Public";
+        public string Info = ">> Public is not restricted." +
+                             "\n>> It specifies that access is not restricted." +
+                             "\n>> Objects that implement public access modifiers are accessible from" +
+                             "\n   everywhere in our project, therefore there is no accessibility restrictions.";
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Details details = new Details();
+
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine(details.Name);
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine(details.Info);
+
+            Console.ReadLine();
+        }
+    }
+}
